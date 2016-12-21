12356ba64182a60e06f2d973b9eff5d2c0d84f62
xiaomi <815090376@qq.com>
2016-12-21 15:36:08 +0800

    Update BDTB.py

--- BDTB.py
+++ BDTB.py
@@ -23,30 +23,33 @@ class Tool:
         return x.strip()
 
 class BDTB:
-    def __init__(self, baseUrl, seeLZ):
+    def __init__(self, baseUrl, seeLZ, floorTag):
         self.baseUrl = baseUrl
         self.seeLZ = '?see_lz='+str(seeLZ)
         self.tool = Tool()
-    def getPage(self, pageNum):
+        self.file = None
+        self.floor = 1
+        self.floorTag = floorTag
+    def getPage(self, page):
         try:
-            url = self.baseUrl + self.seeLZ + '&pn=' + str(pageNum)
+            url = self.baseUrl + self.seeLZ + '&pn=' + str(page)
             request = urllib2.Request(url)
             response = urllib2.urlopen(request)
-            print response.read()
+            return response.read().decode('utf-8')
         except urllib2.URLError, e:
             if hasattr(e, 'reason'):
                 print u'连接百度贴吧失败原因', e.reason
                 return None
-    def getTitle(self):
-        page = self.getPage(1)
+    def getTitle(self,page):
+
         pattern = re.compile('<h3 class="core_title_txt.*?>(.*?)</h3>',re.S)
         result = re.search(pattern,page)
         if result:
             return result.group(1).strip()
         else:
             return None
-    def getPageNum(self):
-        page = self.getPage(1)
+    def getPageNum(self,page):
+
         pattern = re.compile('<li class="l_reply_num".*?</span>.*?<span class="red">(.*?)</span>',re.S)
         result = re.search(pattern,page)
         if result:
@@ -55,9 +58,48 @@ class BDTB:
             return None
     def getContent(self,page):
         pattern = re.compile('<div id="post_content_.*?>(.*?)</div>',re.S)
-        items = re.findall(pattern,page)
-        for item in items:
-            print self.tool.replace(item[1])
+        result = re.findall(pattern,page)
+        contents = []
+        for item in result:
+            content = '\n' + self.tool.replace(item) + '\n'
+            contents.append(content.encode('utf-8'))
+        return contents
+
+    def setFileTitle(self,title):
+        if title is not None:
+            self.file = open(title + '.txt', 'w+')
+        else:
+            self.file = open(self.defaultTitle + '.txt', 'w+')
+    def writeData(self, contents):
+        for item in contents:
+            if self.floorTag == '1':
+                floorLine = '\n' + str(self.floor) + u'---------------------------\n'
+                self.file.write(floorLine)
+            self.file.write(item)
+            self.floor += 1
+    def start(self):
+        indexPage = self.getPage(1)
+        pageNum = self.getPageNum(indexPage)
+        title = self.getTitle(indexPage)
+        self.setFileTitle(title)
+        if pageNum == None:
+            print 'url已经失效，请重试'
+            return
+        try:
+            print '该帖子共有' + str(pageNum) + '页'
+            for i in range(1, int(pageNum)+1):
+                print '正在写入第' + str(i) + '页数据'
+                page = self.getPage(i)
+                contents = self.getContent(page)
+                self.writeData(contents)
+        except IOError,e:
+            print '写入异常，原因' + e.message
+        finally:
+            print '写入任务完成'
+
+print u"请输入帖子代号"
 baseUrl = 'http://tieba.baidu.com/p/3138733512'
-bdtb = BDTB(baseUrl, 1)
-bdtb.getContent(bdtb.getPage(1))
+seeLZ = raw_input("是否只获取楼主发言，是输入1，否输入0\n")
+floorTag = raw_input("是否写入楼层信息，是输入1，否输入0\n")
+bdtb = BDTB(baseUrl, seeLZ, floorTag)
+bdtb.start()
